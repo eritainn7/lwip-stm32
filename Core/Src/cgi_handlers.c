aae4a65a1fe7a2c16f86bbea09fae7112fd6bfb2
eritainn7 <sanyapridava@mail.ru>
2026-07-22 16:44:19 +0300

    feat handler for on all leds

--- Core/Src/cgi_handlers.c
+++ Core/Src/cgi_handlers.c
@@ -20,12 +20,19 @@ const char *led_red_handler(int iIndex, int iNumParams, char *pcParam[], char *p
     HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
     return NULL;
 }
+const char *led_all_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[]) {
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+    return NULL;
+}
 
 // Массив обработчиков
 const tCGI cgi_handlers[] = {
   {"/led_green.cgi", led_green_handler},
   {"/led_blue.cgi", led_blue_handler},
-  {"/led_red.cgi", led_red_handler}
+  {"/led_red.cgi", led_red_handler}, 
+  {"/led_all.cgi", led_all_handler}
 };
 
 // Количество обработчиков
